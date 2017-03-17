d224fb729389e066cde37db894032d1c51386174
andres-morenog <andres.morenog@udea.edu.co>
2017-03-17 12:40:02 -0500

    second commit

---
+++ Lab2Activities/app/src/main/java/lab2/gr06_2017/compumovil/udea/edu/co/lab2activities/Logueo.java
@@ -0,0 +1,13 @@
+package lab2.gr06_2017.compumovil.udea.edu.co.lab2activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Logueo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logueo);
+    }
+}
---
+++ Lab2Activities/app/src/main/java/lab2/gr06_2017/compumovil/udea/edu/co/lab2activities/Splash.java
@@ -0,0 +1,39 @@
+package lab2.gr06_2017.compumovil.udea.edu.co.lab2activities;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        setContentView(R.layout.activity_splash);
+
+
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent().setClass(Splash.this, Logueo.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+
+    }
+}
