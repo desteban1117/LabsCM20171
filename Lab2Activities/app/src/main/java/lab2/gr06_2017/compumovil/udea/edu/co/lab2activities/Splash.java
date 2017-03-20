310d05c69bbfc78f83ddee7eaa3fbb7dbd17fc3b
james garzon <james@experimentality.co>
2017-03-20 15:09:33 -0500

    implement Navigation drawer template

--- Lab2Activities/app/src/main/java/lab2/gr06_2017/compumovil/udea/edu/co/lab2activities/Splash.java
+++ Lab2Activities/app/src/main/java/lab2/gr06_2017/compumovil/udea/edu/co/lab2activities/Splash.java
@@ -27,7 +27,7 @@ public class Splash extends AppCompatActivity {
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                Intent intent = new Intent().setClass(Splash.this, Logueo.class);
+                Intent intent = new Intent().setClass(Splash.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
