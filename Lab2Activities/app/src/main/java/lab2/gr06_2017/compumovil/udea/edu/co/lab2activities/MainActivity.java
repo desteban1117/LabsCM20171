2018924e0e77cd11f121ecde952d633a606b120f
james garzon <james@experimentality.co>
2017-03-20 15:36:26 -0500

    Create profile and events fragments

--- Lab2Activities/app/src/main/java/lab2/gr06_2017/compumovil/udea/edu/co/lab2activities/MainActivity.java
+++ Lab2Activities/app/src/main/java/lab2/gr06_2017/compumovil/udea/edu/co/lab2activities/MainActivity.java
@@ -1,8 +1,10 @@
 package lab2.gr06_2017.compumovil.udea.edu.co.lab2activities;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +15,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import lab2.gr06_2017.compumovil.udea.edu.co.lab2activities.dummy.DummyContent;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, ProfileFragment.OnFragmentInteractionListener, EventFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,19 +83,36 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment = null;
+        Boolean fragmentSelected = false;
 
         if (id == R.id.nav_events) {
-            // Handle the camera action
+            fragment = new EventFragment();
+            fragmentSelected = true;
         } else if (id == R.id.nav_profile) {
-
+            fragment = new ProfileFragment();
+            fragmentSelected = true;
         } else if (id == R.id.nav_config) {
 
         } else if (id == R.id.nav_config) {
 
         }
+        if (fragmentSelected){
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
